e7efbed5755feb2396f2e174bd0b7c2eb1b62c04
Vitor Marques Costa <vmarquec@everis.com>
2020-01-10 11:05:57 -0300

    Bad Solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -32,9 +32,19 @@ public class Reservation {
 		long diff = checkOut.getTime()-checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public void updatedDates(Date checkIn, Date checkOut) {
+	public String updatedDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		
+		if (checkIn.before(now)||checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates.";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date.";
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	@Override
 	public String toString() {
